fa36eb1e8d213843cc1da881954c5ea6a02d47d1
Sanju Raj Prasad <43962657+Blazexsam27@users.noreply.github.com>
2025-02-07 13:07:43 +0530

    Complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 import "./App.css";
 
 function App() {
-  return <div className="container"></div>;
+  return (
+    <div className="container gallery-section">
+      <article className="neon-gallery-section">
+        <img src="/assets/fantasy-1.jfif" alt="fantasy-1" />
+        <img src="/assets/fantasy-2.jfif" alt="fantasy-1" />
+        <img src="/assets/neon3.jpeg" alt="neon3-1" />
+        <img src="/assets/fantasy-4.jfif" alt="fantasy-1" />
+        <img src="/assets/neon1.jpeg" alt="neon1-1" />
+        <img src="/assets/fantasy-7.jfif" alt="fantasy-1" />
+        <img src="/assets/fantasy-6.jfif" alt="fantasy-1" />
+        <img src="/assets/neon8.jpeg" alt="neon8-1" />
+        <img src="/assets/fantasy-5.jfif" alt="fantasy-1" />
+        <img src="/assets/fantasy-3.jfif" alt="fantasy-1" />
+      </article>
+      <div className="bg-title" data-before="">
+        FANTASY ART
+      </div>
+    </div>
+  );
 }
 
 export default App;
